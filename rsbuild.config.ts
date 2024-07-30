061370462eb2141e6c81063dbce7b844d3881e08
Chua Kang Ming <kambing860210@gmail.com>
2024-07-30 18:09:08 +0800

    fix typescript checking

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSass } from '@rsbuild/plugin-sass';
+import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
 
 export default defineConfig({
-  plugins: [pluginReact(), pluginSass()],
+  plugins: [pluginReact(), pluginTypeCheck(), pluginSass()],
 });
