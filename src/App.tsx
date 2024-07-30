edf8b22e38f4ac22cc2f51775acb9bcec0ad7f2c
Chua Kang Ming <kambing860210@gmail.com>
2024-07-30 18:17:18 +0800

    testing todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import TodoAppUsingEventBus from './components/event-bus/TodoAppUsingEventBus';
+import TodoAppUsingJotai from './components/jotai/TodoAppUsingJotai';
 import { useAllFilmsQuery } from './data/starwars/generated';
 
 import './App.css';
 import styles from './App.module.scss';
+import { useState } from 'react';
 
 const App = () => {
   const { data } = useAllFilmsQuery();
   console.log({ data });
+  const [ary, setAry] = useState<number[]>([]);
   return (
     <div className="content">
       <h1 className={styles.title}>Rsbuild with React</h1>
@@ -13,6 +17,20 @@ const App = () => {
         Start building amazing things with Rsbuild.
       </p>
       <div>My content</div>
+      {/* {ary.map((key) => (
+        <TodoAppUsingEventBus key={key} />
+      ))}
+      {ary.map((key) => (
+        <TodoAppUsingJotai key={key} />
+      ))}
+      <button
+        type="button"
+        onClick={() => {
+          setAry((prev) => [...prev, prev.length]);
+        }}
+      >
+        Add new app
+      </button> */}
     </div>
   );
 };
